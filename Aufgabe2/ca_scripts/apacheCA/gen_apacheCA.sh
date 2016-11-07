80d7e2379d52080c3b7ca834cf409f2b55644db0
yavuz <you@example.com>
2016-11-07 21:59:17 +0100

    afg. 2

---
+++ Aufgabe2/ca_scripts/apacheCA/gen_apacheCA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+APACHE="diana-yavuz.informatik.haw-hamburg.de"
+
+# key erzeugen
+openssl genrsa -out $APACHE.key 2048
+
+# csr erzeugen
+openssl req -new -key $APACHE.key -out $APACHE.csr -subj "/C=DE/O=haw-hamburg/OU=informatik/CN=diana-yavuz.informatik.haw-hamburg.de"
+
+# zertifikat signieren
+openssl ca -config sslCA.cnf -gencrl -in $APACHE.csr -out $APACHE.crt
+
---
+++ Aufgabe2/ca_scripts/sslCA/gen_sslCA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SSL="sslCA"
+
+# key erzeugen
+openssl genrsa -out $SSL.key 2048
+
+# csr erzeugen
+openssl req -new -key $SSL.key -out $SSL.csr -subj "/C=DE/O=haw-hamburg/OU=informatik/CN=CA-diana-yavuz"
+
+# zertifikat signieren
+openssl ca -config rootCA/root.cnf -gencrl -in $SSL.csr -out $SSL.crt
+
